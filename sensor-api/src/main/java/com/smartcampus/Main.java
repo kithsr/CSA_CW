05378705037f01ce2bed41c3e66487188f70096a
A.W.Kithsara <withzz1234@gmail.com>
2026-04-12 17:49:16 +0530

    Scan com.smartcampus package for resources
    Change ResourceConfig to scan 'com.smartcampus' instead of 'com.smartcampus.resources' so JAX-RS will discover resource classes across the whole package hierarchy. Ensures resources placed outside the previous 'resources' subpackage are registered; Main.class was recompiled accordingly.

--- sensor-api/src/main/java/com/smartcampus/Main.java
+++ sensor-api/src/main/java/com/smartcampus/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static final String BASE_URI = "http://localhost:8080/api/v1/";
 
     public static HttpServer startServer() {
-        final ResourceConfig rc = new ResourceConfig().packages("com.smartcampus.resources");
+        final ResourceConfig rc = new ResourceConfig().packages("com.smartcampus");
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
