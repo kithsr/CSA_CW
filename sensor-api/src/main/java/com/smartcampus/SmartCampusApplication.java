ce48b4c6dd97677ae88ff032c864ee261dda3617
A.W.Kithsara <withzz1234@gmail.com>
2026-04-22 18:47:38 +0530

    Register JAX-RS Application in web.xml
    Remove the @ApplicationPath annotation and its import from SmartCampusApplication, and configure the JAX-RS application explicitly in WEB-INF/web.xml by setting the javax.ws.rs.Application init-param to com.smartcampus.SmartCampusApplication. This moves JAX-RS registration from annotation-based scanning to an explicit servlet configuration. Updated compiled class artifact accordingly.

--- sensor-api/src/main/java/com/smartcampus/SmartCampusApplication.java
+++ sensor-api/src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -1,8 +1,6 @@
 package com.smartcampus;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api/v1")
 public class SmartCampusApplication extends Application {
 }
